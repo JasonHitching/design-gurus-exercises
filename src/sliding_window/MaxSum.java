858350f473998c5aa2b43a77097631f0607dd6d4
Jason Hitching <jasonhitching1994@gmail.com>
2022-01-13 09:38:54 +0000

    <implemented> brute force solution
    - Brute force solution of max sum of sub-array of size k implemented

--- src/sliding_window/MaxSum.java
+++ src/sliding_window/MaxSum.java
@@ -9,4 +9,32 @@ package sliding_window;
  */
 public class MaxSum {
 
+    // Constructor
+    public MaxSum () {};
+
+    /**
+     * Brute force algorithm for solving the problem
+     *
+     * @param numArray array containing integers for analysis
+     * @param k desired size of sub array
+     * @return maximum sum of 'k' sized sub array
+     */
+    public int findMaxSumK(int[] numArray, int k) {
+        int maxSum = 0;
+
+        // Loop array.length - k to prevent nested loop from going out of bounds
+        for(int i = 0;  i < numArray.length - k; i++) {
+            int kSum = 0; // Variable for tracking current sub-array sum
+            // 'i + k' moves through the array in sub-array sizes of 'k'
+            for (int j = i; j < i + k; j++) {
+                kSum += numArray[j];
+            }
+            if (kSum > maxSum) {
+                maxSum = kSum;
+            }
+        }
+        return maxSum;
+    }
+
+
 }
