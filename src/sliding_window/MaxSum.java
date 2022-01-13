52e3b81cbcf552e28f2099ce947c0671f598c4eb
Jason Hitching <jasonhitching1994@gmail.com>
2022-01-13 08:56:49 +0000

    Initial Commit

---
+++ src/sliding_window/MaxSum.java
@@ -0,0 +1,12 @@
+package sliding_window;
+/**
+ * Given an array of positive numbers and a positive number ‘k,’
+ * find the maximum sum of any contiguous sub-array of size ‘k’.
+ *
+ * Input: [2, 1, 5, 1, 3, 2], k=3
+ * Output: 9
+ * Explanation: Sub-array with maximum sum is [5, 1, 3].
+ */
+public class MaxSum {
+
+}
